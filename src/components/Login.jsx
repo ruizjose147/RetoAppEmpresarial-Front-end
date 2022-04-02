7de478f73cab548c5f5de3f9637423ddd75b323f
ruizjose147 <ruizjose_147@hotmail.com>
2022-04-02 00:15:44 -0500

    Validaciones en los inputs y pintando errores

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,87 @@
 import React from 'react'
+import { useState } from 'react';
 
 export const Login = () => {
 
-    const [email, setEmail] = React.useState('')
-    const [pass, setPass] = React.useState('')
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [error, setError] = useState(null);
+    const [esRegistro, setEsRegistro] = useState(true)
+
+    const procesarDatos = e =>{
+        e.preventDefault();
+
+        if(!email.trim()){
+            //console.log("ingrese email")
+            setError("Ingrese un email")
+            return
+        }
+
+        if(!pass.trim()){
+            //console.log('ingrese password')
+            setError('ingrese password')
+            return
+        }
+        if(pass.length<6){
+            //console.log("pass min 6")
+            setError("El password debe ser de minimo 6 caracteres")
+        }
+        console.log("pasando todas las validaciones")
+        setError(null)
+    }
 
   return (
     <div className="mt-5">
-        <h3 className="text-center">Accerder o Registrarse</h3>
+        <h3 className="text-center">
+            {
+                esRegistro ? "Registro Usuario" : "Login de Acceso"
+            }
+        </h3>
         <hr />
         <div className="row justify-content-center">
             <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                <form>
+                <form onSubmit={procesarDatos}>
+                    {
+                        error && (
+                        <div className="alert alert-danger">
+                            {error}
+                        </div>   
+                        )
+                    }
                     <input 
                     type="email"
                     className="form-control mb-2"
                     placeholder="Email"
                     onChange={e => setEmail(e.target.value)}
+                    value={email}
                     />
                     <input 
                     type="password"
                     className="form-control mb-2"
                     placeholder="Password"
                     onChange={e => setPass(e.target.value)}
+                    value={pass}
                     />
+                    <div className="row justify-content-center">
                     <button 
-                        className="btn btn-lg btn-primary btn-block"
+                        className="btn btn-md btn-primary btn-block"
                         type="submit"
                     >
-                        Registrarse
+                        {
+                            esRegistro ? "Registrarme" : "Ingresar"
+                        }
                     </button>
+                    <br />
                     <button 
-                        className="btn btn-sm btn-info btn-block"
+                        className="btn btn-sm btn-info btn-block mt-2"
+                        onClick={()=> setEsRegistro(!setEsRegistro)}
                         type="button"
                     >
-                        ¿Ya estas Registrado?
+                        {
+                            esRegistro ? "¿Estas Registrado?" : "¿No tienes Cuenta?"
+                        }
                     </button>
+                    </div>
                 </form>
             </div>
         </div>
