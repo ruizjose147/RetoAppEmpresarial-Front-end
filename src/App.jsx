725e249012ab5d45339959e1f3c263eb22c050c9
ruizjose147 <ruizjose_147@hotmail.com>
2022-04-01 20:06:34 -0500

    Creacion del proyecto y configuracion inicial para autenticacion con firebase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Auth</h1>
+    </div>
+  );
+}
+
+export default App;
