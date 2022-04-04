1cecc45edbac429849b0d58e2ad4e735bd01b136
ruizjose147 <ruizjose_147@hotmail.com>
2022-04-04 14:43:23 -0500

    Organizando carpetas

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import { Navbar } from "./components/Navbar";
-import Vendedor from "./components/Vendedor";
-import app from './firebase/firebase';
+import Login from "../components/Login";
+import { Navbar } from "../components/Navbar";
+import Vendedor from "../components/Vendedor";
+import app from '../firebase/firebase';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth(app);
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import generateStore from './redux/store';
+import generateStore from '../redux/store';
 import { Provider } from 'react-redux';
 
 const store = generateStore()
